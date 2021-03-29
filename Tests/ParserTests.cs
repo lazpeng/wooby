0ff100f32e9f2dd27fc3718bc4cadf6eeface8d5
Rafael Nakano <rafael.nakano@maximatech.com.br>
2021-03-29 18:33:02 -0300

    A few project changes, added tests and bug fixes to the Parser

---
+++ Tests/ParserTests.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wooby;
+
+namespace Tests
+{
+    [TestClass]
+    public class ParserTests
+    {
+        [TestMethod]
+        public void TestOperatorParsing()
+        {
+            var input = "   +   ";
+
+            var next = Parser.NextToken(input, 0);
+            Assert.AreEqual(next.Kind, Parser.TokenKind.Operator);
+            Assert.AreEqual(next.OperatorValue, Operator.Plus);
+        }
+
+        [TestMethod]
+        public void TestTokenDetection()
+        {
+            var input = " 'testing' SELECT 2.45 symbol";
+
+            var next = Parser.NextToken(input, 0);
+            Assert.AreEqual(next.Kind, Parser.TokenKind.LiteralString);
+
+            int offset = next.InputLength;
+
+            next = Parser.NextToken(input, offset);
+            Assert.AreEqual(next.Kind, Parser.TokenKind.Keyword);
+            offset += next.InputLength;
+
+            next = Parser.NextToken(input, offset);
+            Assert.AreEqual(next.Kind, Parser.TokenKind.LiteralNumber);
+            offset += next.InputLength;
+
+            next = Parser.NextToken(input, offset);
+            Assert.AreEqual(next.Kind, Parser.TokenKind.Symbol);
+        }
+
+        [TestMethod]
+        public void TestString()
+        {
+            var input = " 'test string' ";
+
+            var next = Parser.NextToken(input, 0);
+            Assert.AreEqual(next.Kind, Parser.TokenKind.LiteralString);
+            Assert.AreEqual(next.StringValue, "test string");
+        }
+
+        [TestMethod]
+        public void TestSymbol()
+        {
+            var input = " test another ";
+
+            var next = Parser.NextToken(input, 0);
+            Assert.AreEqual(next.Kind, Parser.TokenKind.Symbol);
+            Assert.AreEqual(next.StringValue, "test");
+        }
+
+        [TestMethod]
+        public void TestNumber()
+        {
+            var input = " 3.14 ";
+
+            var next = Parser.NextToken(input, 0);
+            Assert.AreEqual(next.Kind, Parser.TokenKind.LiteralNumber);
+            Assert.AreEqual(next.NumberValue, 3.14);
+
+            input = " 5";
+
+            next = Parser.NextToken(input, 0);
+            Assert.AreEqual(next.Kind, Parser.TokenKind.LiteralNumber);
+            Assert.AreEqual(next.NumberValue, 5);
+
+            input = " 4.99e5";
+
+            next = Parser.NextToken(input, 0);
+            Assert.AreEqual(next.Kind, Parser.TokenKind.LiteralNumber);
+            Assert.AreEqual(next.NumberValue, 499000);
+        }
+
+        [TestMethod]
+        public void TestKeyword()
+        {
+            var input = " WHERE ";
+
+            var next = Parser.NextToken(input, 0);
+            Assert.AreEqual(next.Kind, Parser.TokenKind.Keyword);
+            Assert.AreEqual(next.KeywordValue, Keyword.Where);
+        }
+
+        [TestMethod]
+        public void TestPonctuation()
+        {
+            var input = " , ";
+
+            var next = Parser.NextToken(input, 0);
+            Assert.AreEqual(next.Kind, Parser.TokenKind.Comma);
+        }
+    }
+}
