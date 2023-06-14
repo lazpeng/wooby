e6693dc0f11a2e45c98c3cbaf330d6a07ebbdb90
Rafael Nakano <lazpeng@gmail.com>
2023-06-14 02:14:04 -0300

    Initial commit, a few classes here and there, nothing works yet

---
+++ Compiler.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wooby
+{
+    public enum Operator
+    {
+        Plus,
+        Minus,
+        ForwardSlash,
+        Asterisk,
+        ParenthesisLeft,
+        ParenthesisRight,
+        Power,
+        LessThan,
+        MoreThan,
+        Equal,
+    }
+
+    public enum Keyword
+    {
+        Select,
+        From,
+        Where,
+        Is,
+        Not,
+        Null,
+        True,
+        False,
+        And,
+        Or,
+        Asc,
+        Desc,
+        Order,
+        By
+    }
+
+    public class Expression
+    {
+        public enum NodeKind
+        {
+            Operator,
+            String,
+            Number,
+            Boolean,
+            Reference
+        }
+
+        public class Node
+        {
+            public NodeKind Kind;
+            public string StringValue;
+            public double NumberValue;
+            public Operator OperatorValue;
+            public ColumnReference ReferenceValue;
+        }
+
+        public enum ExpressionType
+        {
+            Unknown,
+            Number,
+            String,
+            Boolean
+        }
+
+        private string _identifier;
+
+        public string FullText { get; set; }
+        public string Identifier
+        {
+            get
+            {
+                return _identifier ?? FullText;
+            }
+            set
+            {
+                _identifier = value;
+            }
+        }
+        public List<Node> Nodes { get; set; } = new List<Node>();
+        public ExpressionType TypeGuess { get; set; } = ExpressionType.Unknown;
+    }
+
+    public class TableReference
+    {
+        public string Schema { get; set; } = "";
+        public string Table { get; set; } = "";
+        public int InputLength { get; set; }
+    }
+
+    public class ColumnReference : TableReference
+    {
+        public string Column { get; set; } = "";
+    }
+
+    public enum CommandKind
+    {
+        Pragma,
+        Query,
+        Manipulation,
+        Definition
+    }
+
+    public enum CommandClass
+    {
+        Select,
+        Update,
+        Delete,
+        Alter,
+        Create,
+    }
+
+    public abstract class Command
+    {
+        public CommandKind Kind { get; protected set; }
+        public CommandClass Class { get; protected set; }
+        public string OriginalText { get; set; }
+    }
+
+    public enum OrderingKind
+    {
+        Ascending,
+        Descending
+    }
+
+    public class Ordering
+    {
+        public Expression OrderExpression { get; set; }
+        public OrderingKind Kind { get; set; } = OrderingKind.Ascending;
+    }
+
+    public class SelectCommand : Command
+    {
+        public SelectCommand()
+        {
+            Kind = CommandKind.Query;
+            Class = CommandClass.Select;
+        }
+
+        public List<Expression> OutputColumns { get; private set; } = new List<Expression>();
+        public TableReference MainSource { get; set; }
+        public Expression FilterConditions { get; set; }
+        public Ordering OutputOrder { get; set; }
+    }
+
+    public class Compiler
+    {
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# Wooby
+
+My take on a database, without prior knowledge or guidance on the subject.
+I'll keep this very short so basically I'm doing it in C# .NET for practicing my skills
+with the ecosystem, and this is *not* meant to be taken seriously, to be performing,
+feature complete or even accurate to the standards at the moment, just a funny side
+project to see how hard it is to get to a somewhat barebones database.
+
+My "roadmap" is as follows:
+
+## v0.1
+* Be able to run a simple select statement and get a simple result back, like CURRENT_DATE or selecting from a hard coded in-memory table for testing purposes. This means there's a little bit of everything working already, from lexer to the actual "virtual machine" executing the statement and I can start adding more features
+
+## v0.2
+* Again on SELECTs but more feature complete this time, with support for filtering and ordering the results. Also some virtual tables containing metadata information, not hardcoded but with actual data from the running schema
+
+## v0.3
+* Support for creating and dropping tables, inserting, updating and deleting rows from it. At this stage everything is still in-memory and no database data is written to the disk or persisted in any way
+
+### Current stage: v0.0
+
+I'll add more stuff as the project advances
